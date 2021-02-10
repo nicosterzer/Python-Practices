62e9967b9014bf282a012d37e5b4710998c42795
nicosterzer <76760798+nicosterzer@users.noreply.github.com>
2021-02-10 09:37:30 -0300

    First JavaScript practice

---
+++ Hello world and variables.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+
+alert("Hello world");
+
+let name;
+
+name = "Nicolas Sterzer";
+
+var age = 19;
+
+console.log(name, age);
+
+let dateOfBirth = 3.2001;
+
+console.log("My birthday is December", dateOfBirth);
